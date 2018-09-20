e91d6b4658fcfdb2ef6b4993d043e8fd761ccd03
Harshit Sharma <43315578+sharma1612harshit@users.noreply.github.com>
2018-09-20 19:29:10 +0530

    An amazing sorting algorithm

---
+++ bogosort.py
@@ -0,0 +1,29 @@
+def bogoSort(a):
+    n = len(a)
+    while (is_sorted(a) == False):
+        shuffle(a)
+
+def is_sorted(a):
+    n = len(a)
+    for i in range(0, n - 1):
+        if (a[i] > a[i + 1]):
+            return False
+    return True
+
+def shuffle(a):
+    n = len(a)
+    for i in range(0, n):
+        r = random.randint(0, n - 1)
+        a[i], a[r] = a[r], a[i]
+
+n=9
+arr=[]
+import random, time
+for i in range (0,n):
+    index=random.randint(0,100)
+    arr.append(index)
+start=time.time()
+bogoSort(arr)
+print("Sorted array - ")
+print(arr)
+print("Sorting time: {} sec".format(time.time()-start))
