e0742085d3218346209ce431f5da2be6f36dcfda
Harshit Sharma <sharma1612harshit@gmail.com>
2019-06-17 15:10:46 +0530

    Get system port status

---
+++ get_port_status.py
@@ -0,0 +1,23 @@
+import time
+
+def epoch_to_readable(epoch):
+    readable=time.strftime("%d/%m/%y - %H:%M:%S",epoch)
+    return(readable)
+
+def timestamp():
+    time_s=epoch_to_readable(time.localtime())
+    return (time_s)
+
+def port_status():
+    cmd_out = subprocess.Popen(['netstat','-ap','tcp'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
+
+    writer = open("cmd.txt", "a")
+    string_to_write = "\n" + "-------------------------" + "\n" + str(timestamp()) + "\n" + "-------------------------"+"\n"
+    writer.write(string_to_write)
+    writer.close()
+
+    writer = open("cmd.txt", "a")
+    writer.write(cmd_out[0])
+    writer.close()
+
+port_status()
