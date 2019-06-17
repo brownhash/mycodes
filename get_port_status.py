ee655db02c28ad1ad66f14e242e4171652df5dc4
Harshit Sharma <sharma1612harshit@gmail.com>
2019-06-17 15:13:09 +0530

    Get port status

--- get_port_status.py
+++ get_port_status.py
@@ -1,23 +1,25 @@
 import time
 
-def epoch_to_readable(epoch):
-    readable=time.strftime("%d/%m/%y - %H:%M:%S",epoch)
-    return(readable)
+def ports():
+    def epoch_to_readable(epoch):
+        readable=time.strftime("%d/%m/%y - %H:%M:%S",epoch)
+        return(readable)
 
-def timestamp():
-    time_s=epoch_to_readable(time.localtime())
-    return (time_s)
+    def timestamp():
+        time_s=epoch_to_readable(time.localtime())
+        return (time_s)
 
-def port_status():
-    cmd_out = subprocess.Popen(['netstat','-ap','tcp'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
+    def port_status():
+        cmd_out = subprocess.Popen(['netstat','-ap','tcp'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
 
-    writer = open("cmd.txt", "a")
-    string_to_write = "\n" + "-------------------------" + "\n" + str(timestamp()) + "\n" + "-------------------------"+"\n"
-    writer.write(string_to_write)
-    writer.close()
+        writer = open("cmd.txt", "a")
+        string_to_write = "\n" + "-------------------------" + "\n" + str(timestamp()) + "\n" + "-------------------------"+"\n"
+        writer.write(string_to_write)
+        writer.close()
 
-    writer = open("cmd.txt", "a")
-    writer.write(cmd_out[0])
-    writer.close()
+        writer = open("cmd.txt", "a")
+        writer.write(cmd_out[0])
+        writer.close()
 
-port_status()
+    result=port_status()
+    return(result)
