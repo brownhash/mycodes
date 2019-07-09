a30ee0851cbdddf6c5b01209e891816104119b03
Harshit Sharma <sharma1612harshit@gmail.com>
2019-07-09 15:40:41 +0530

    Get number of reserved instances per instance type

---
+++ get_reserved_instnces_per_instance_type.py
@@ -0,0 +1,10 @@
+import boto3
+
+def get_reservedInstances(region_name):
+    ec2 = boto3.client('ec2', region_name=region_name)
+    Reserved_instances = ec2.describe_reserved_instances()
+    instances = Reserved_instances.get('ReservedInstances')
+    result = {}
+    for i in instances:
+        result[i.get('InstanceType')] = i.get('InstanceCount')
+    return(result)
