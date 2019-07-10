5a9635b2d03cd91eedafd728e36d109adb843934
Harshit Sharma <sharma1612harshit@gmail.com>
2019-07-10 14:47:54 +0530

    Raise an alert when the number of RI > OnDemand instances

--- comapairing_ri_and_ondemand.py
+++ comapairing_ri_and_ondemand.py
@@ -1,24 +1,25 @@
 from aws_instances_per_instance_type import getInstances
 from aws_reserved_instnces_per_instance_type import get_reservedInstances
 from aws_spot_instances_per_instance_type import get_spotInstances
+from aws_instance_equivalence import instanceEquivalence
 
-regions=['ap-southeast-1','ap-south-1']
+def alert(region):
 
-total_instances={}
-total_spot={}
-total_reserved={}
-ondemand={}
+    total_instances = {}
+    total_spot = {}
+    total_reserved = {}
+    ondemand = {}
+    alert = {}
 
-for i in regions:
-    instances=getInstances(i)
+    instances = getInstances(region)
     for j in instances:
-        if(total_instances.get(j)):
-            val=total_instances.get(j)+instances.get(j)
-            total_instances[j]=val
+        if (total_instances.get(j)):
+            val = total_instances.get(j) + instances.get(j)
+            total_instances[j] = val
         else:
-            total_instances[j]=instances.get(j)
+            total_instances[j] = instances.get(j)
 
-    spot = get_spotInstances(i)
+    spot = get_spotInstances(region)
     for k in spot:
         if (total_spot.get(k)):
             val2 = total_spot.get(k) + spot.get(k)
@@ -26,7 +27,7 @@ for i in regions:
         else:
             total_spot[k] = spot.get(k)
 
-    reserved = get_reservedInstances(i)
+    reserved = get_reservedInstances(region)
     for x in reserved:
         if (total_reserved.get(x)):
             val3 = total_reserved.get(x) + reserved.get(x)
@@ -34,25 +35,27 @@ for i in regions:
         else:
             total_reserved[x] = reserved.get(x)
 
+    # print("\nNormal Ec2")
+    # print(total_instances)
+    # print("\nSpot Instances")
+    # print(total_spot)
 
-print("Reserved Instances")
-print(total_reserved)
-#print("\nNormal Ec2")
-#print(total_instances)
-#print("\nSpot Instances")
-#print(total_spot)
+    for i in total_spot:
+        if (not total_instances.get(i)):
+            ondemand[i] = total_spot.get(i)
 
+    for i in total_instances:
+        if (total_spot.get(i)):
+            val = total_instances.get(i) - total_spot.get(i)
+            ondemand[i] = val
+        else:
+            ondemand[i] = total_instances.get(i)
 
-for i in total_spot:
-    if(not total_instances.get(i)):
-        ondemand[i]=total_spot.get(i)
+    equated_ondemand = instanceEquivalence(ondemand)
+    equated_reserved = instanceEquivalence(total_reserved)
 
-for i in total_instances:
-    if(total_spot.get(i)):
-        val=total_instances.get(i)-total_spot.get(i)
-        ondemand[i]=val
-    else:
-        ondemand[i]=total_instances.get(i)
+    for i in equated_reserved:
+        if (equated_reserved.get(i) > equated_ondemand.get(i)):
+            alert[i] = equated_reserved.get(i) - equated_ondemand.get(i)
 
-print("\nOndemand")
-print(ondemand)
+    return(alert)
