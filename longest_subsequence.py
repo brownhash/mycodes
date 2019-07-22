8a97980fe0ed957d0d37c3f4dd043ad3d4ed1476
Harshit Sharma <you@example.com>
2019-07-22 16:57:01 +0530

    Calculate the longest continuous substring

---
+++ longest_subsequence.py
@@ -0,0 +1,37 @@
+"""
+Calculate the longest substring containing a sequence of continuous numbers.
+
+Example:
+    input: 1231234
+    output: 1234
+    Explanation: 
+        it has 2 substrings containing a sequence of continuous numbers - 123 and 1234
+        As 1234 is greater in length hence 1234 is the answer
+    
+    input: 1231232345
+    output: 2345
+            
+    input: 123123123
+    output: 123
+"""
+
+def substring(a):
+    a = str(a)
+    substring = ""
+    temp = ""
+
+    for i in a:
+        if (len(temp) > 0):
+            check = temp[len(temp) - 1]
+            elem = i
+            if (int(elem) == int(check) + 1):
+                temp += elem
+            else:
+                temp = ""
+                temp += elem
+        else:
+            temp += i
+        if (len(temp) > len(substring)):
+            substring = temp
+
+    return(substring)
