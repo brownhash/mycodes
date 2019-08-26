c473c51f0a3cae643b380b5074050874d559ca0e
Harshit Sharma <sharma1612harshit@gmail.com>
2019-08-26 16:38:31 +0530

    loops in java

---
+++ JAVA/basics/loops.java
@@ -0,0 +1,15 @@
+class Loops{
+    public static void main(String[] args) {
+        // for loop
+        System.out.println("-- for --");
+        for(int i=0; i<10; i++){
+            System.out.println(i);
+        }
+        System.out.println("-- while --");
+        int x = 10;
+        while(x > 5){
+            System.out.println(x);
+            x--;
+        }
+    }
+}
