73d5542fadc9c8657e0689555d60e59f55bac67d
Harshit Sharma <sharma1612harshit@gmail.com>
2019-08-26 17:16:37 +0530

    Major element in an array

--- JAVA/codes/array_major.java
+++ JAVA/codes/array_major.java
@@ -1,25 +1,39 @@
 // calculating the major elements in an array
-// major element is the one divisible by 3 & 7
+// major element is the one which occurs the most in an array
 
 import java.io.*;
 import java.util.*;
+import java.util.Random;
 
 class ArrayMajor{
     public static void main(String[] args) {
         // int arr[] = new int[100];
         List<Integer> arr = new ArrayList<Integer>();
-        List<Integer> arr_list = new ArrayList<Integer>();
+        List<Integer> arr_count = new ArrayList<Integer>();
+        Random rand = new Random();
 
         for(int i = 0; i < 100; i++){
-            arr.add(i+1);
+            arr.add(rand.nextInt(1000));
         }
         for(int i = 0; i < 100; i++){
             int elem = arr.get(i);
-            if((elem % 3 == 0) && (elem % 7 == 0)){
-                arr_list.add(elem);
+            int count = 0;
+            for(int j = 0; j < 100; j++) {
+                int temp = arr.get(j);
+                if(temp == elem){
+                    count ++;
+                }
+            }
+            arr_count.add(count);
+        }
+        int max = arr_count.get(0);
+        int index = 0;
+        for(int i = 0; i < 100; i++){
+            if(arr_count.get(i) > max){
+                index = i;
+                max = arr_count.get(i);
             }
         }
-        System.out.println("Major elements");
-        System.out.println(arr_list);
+        System.out.println(arr.get(index));
     }
 }
