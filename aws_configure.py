23d32b49a81fc4f5b0dfb968f59594179b55b3a9
Harshit Sharma <sharma1612harshit@gmail.com>
2019-08-29 15:27:46 +0530

    Configure aws-cli

--- aws_configure.py
+++ aws_configure.py
@@ -2,7 +2,21 @@ import sys
 
 
 def get_help():
-    print("\nPlease add some options\n")
+    print("Error: No options found")
+    print("Usage:\n\tpython aws_configure.py [option]\n"
+          "\tPossible options: -h, -option1, -option2\n")
+
+
+config = {
+    "acc1": {
+        "access": "string",
+        "secret_access": "string"
+    },
+    "acc2": {
+        "access": "string",
+        "secret_access": "string"
+    },
+}
 
 
 def create_config(access_key, secret_access_key):
@@ -16,21 +30,29 @@ def create_config(access_key, secret_access_key):
     writer.close()
 
 
-if not sys.argv[1]:
-    get_help()
-else:
+try:
     option = sys.argv[1]
-    if option == "-r":
-        access_key = ""
-        secret_access_key = ""
+    if option == "-option1":
+        access_key = config.get('acc1').get('access')
+        secret_access_key = config.get('acc1').get('secret_access')
 
         create_config(access_key, secret_access_key)
 
-    elif option == "-p":
-        access_key = ""
-        secret_access_key = ""
+        print("AWS Config successful for Account 1")
+
+    elif option == "-option2":
+        access_key = config.get('acc2').get('access')
+        secret_access_key = config.get('acc2').get('secret_access')
 
         create_config(access_key, secret_access_key)
+
+        print("AWS Config successful for Account 2")
+
+    elif option == "-h":
+        get_help()
 
     else:
         get_help()
+
+except:
+    get_help()
