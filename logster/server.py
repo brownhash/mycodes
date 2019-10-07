38abe0ec2dbeb7f632c90cff2710a2ac8b8c4161
Harshit Sharma <sharma1612harshit@gmail.com>
2019-10-07 11:55:57 +0530

    added uwsgi conf

--- logster/server.py
+++ logster/server.py
@@ -1,16 +1,21 @@
 from flask import Flask
 from system_metrics import get_metrics
 import time
+import os
+
+config = eval(open('config', 'r').read())
+cpu_limit = config['cpu_limit']
 
 app = Flask(__name__)
 @app.route("/metrics")
 def server():
     data = get_metrics()
     cpu = data['cpu']
-    if cpu > 4:
-        message = "{}\n{}\n{}\n{} - {}%".format("--------", time.strftime("%H:%M:%S"), "--------", "ALERT- cpu load high!", cpu)
-        print(message)
+    if cpu > cpu_limit:
+        message = "{} - {} ({}%)".format(time.strftime("%d/%m/%y %H:%M:%S"), "ALERT: cpu load high!", cpu)
+        sep = "-"*len(message)
+        print("{}\n{}\n{}".format(sep, message, sep))
     return data
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0')
