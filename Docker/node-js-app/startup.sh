85ba6ae444adf375d593074a2889febcb1e1ba08
sharma1612harshit <sharma1612harshit@gmail.com>
2019-11-23 12:05:36 +0530

    Docker applications

---
+++ Docker/node-js-app/startup.sh
@@ -0,0 +1,2 @@
+docker build -t sharma1612harshit/basicnpmapp .
+docker run -p 8080:8080 sharma1612harshit/basicnpmapp
---
+++ Docker/redis/startup.sh
@@ -0,0 +1,2 @@
+docker build -t sharma1612harshit/redis .
+docker run sharma1612harshit/redis
