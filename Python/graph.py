f192f460cef4b06cf54acd2c4d072ade53b531da
sharma1612harshit <sharma1612harshit@gmail.com>
2020-01-09 20:47:16 +0530

    weighted graph in python

--- Python/graph.py
+++ Python/graph.py
@@ -1,30 +1,72 @@
-from collections import defaultdict
+class Graph:
+    def __init__(self):
+        self.node = []
+        self.connections = {}
 
-graph=defaultdict(list)
+    def addnode(self, node):
+        self.node.append(node)
 
-def addEdge(graph,u,v):
-    graph[u].append(v)
+    def connect(self, node1, node2, weight):
+        if type(weight).__name__ != "int" and type(weight).__name__ != "float":
+            raise Exception("Weight can only be a integer value and not {}".format(type(weight).__name__))
+        if node1 in self.node and node2 in self.node:
+            if node1 in self.connections:
+                all_nodes = self.connections.get(node1)
+                all_nodes[node2] = weight
+            else:
+                self.connections[node1] = {node2: weight}
+            if node2 in self.connections:
+                all_nodes = self.connections.get(node2)
+                all_nodes[node1] = weight
+            else:
+                self.connections[node2] = {node1: weight}
+        else:
+            raise Exception("{} - {} is not a valid path".format(node1, node2))
 
-def createEdges(graph):
-    edges=[]
+    def pathweight(self, node1, node2):
+        if node1 in self.connections and node2 in self.connections:
+            connection1 = self.connections.get(node1)
+            if node2 in connection1:
+                return connection1.get(node2)
+            else:
+                raise Exception("{} & {} are not connected".format(node1, node2))
+        else:
+            raise Exception("{} - {} is not a valid path".format(node1, node2))
 
-    for node in graph:
-        for neighbour in graph[node]:
-            edges.append((node,neighbour))
+    def connection(self, node):
+        if node in self.connections:
+            result = []
+            for connection_nodes in self.connections.get(node):
+                result.append(connection_nodes)
+            return result
+        else:
+            raise Exception("{} is not a valid node".format(node))
 
-    return edges
+    def connectionmap(self, node):
+        if node in self.connections:
+            return self.connections.get(node)
+        else:
+            raise Exception("{} is not a valid node".format(node))
 
+    def nodes(self):
+        return self.node
 
-addEdge(graph, 'a', 'c')
-addEdge(graph, 'b', 'c')
-addEdge(graph, 'b', 'e')
-addEdge(graph, 'c', 'd')
-addEdge(graph, 'c', 'e')
-addEdge(graph, 'c', 'a')
-addEdge(graph, 'c', 'b')
-addEdge(graph, 'e', 'b')
-addEdge(graph, 'd', 'c')
-addEdge(graph, 'e', 'c')
+    def __str__(self):
+        return self.__class__.__name__
 
-print(createEdges(graph))
-print(graph)
+# Using Graph -
+# 1. Create isinstance
+#     graph = Graph()
+# 2. Add nodes
+#     graph.addnode("Node1")
+#     graph.addnode("Node1")
+# 3. Connect nodes with a weight of their path
+#     graph.connect("Node1", "Node2", 2)
+# 4. Get path weight of 2 nodes
+#     graph.pathweight("Node1", "Node2")
+# 5. Get connected nodes
+#     graph.connection("Node1")
+# 6. Get connected nodes with distance
+#     graph.connectionmap("Node1")
+# 7. Get all added nodes
+#     graph.nodes()
